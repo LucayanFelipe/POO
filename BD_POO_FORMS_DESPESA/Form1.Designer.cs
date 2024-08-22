3227ad478123ed343fbb81cad8621a306a93be7b
LucayanFelipe <161391775+LucayanFelipe@users.noreply.github.com>
2024-08-21 20:54:48 -0400

    Add files via upload

---
+++ BD_POO_FORMS_DESPESA/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace BD_POO_FORMS_DESPESA
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            bt_cadastrarFuncionario = new Button();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            bt_cadastrarCaixa = new Button();
+            bt_cadastrarFornecedor = new Button();
+            bt_cadastrarDespesa = new Button();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // bt_cadastrarFuncionario
+            // 
+            bt_cadastrarFuncionario.BackColor = SystemColors.ActiveCaptionText;
+            bt_cadastrarFuncionario.FlatAppearance.BorderColor = Color.Red;
+            bt_cadastrarFuncionario.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            bt_cadastrarFuncionario.ForeColor = Color.Lime;
+            bt_cadastrarFuncionario.Location = new Point(285, 125);
+            bt_cadastrarFuncionario.Name = "bt_cadastrarFuncionario";
+            bt_cadastrarFuncionario.Size = new Size(194, 67);
+            bt_cadastrarFuncionario.TabIndex = 0;
+            bt_cadastrarFuncionario.Text = "CADASTRAR FUNCIONARIO";
+            bt_cadastrarFuncionario.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            label1.Font = new Font("Segoe Print", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(343, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(314, 48);
+            label1.TabIndex = 1;
+            label1.Text = "MENU DE OPÇÕES";
+            label1.TextAlign = ContentAlignment.TopCenter;
+            label1.Click += label1_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.Alya_Sometimes_Hides_Her_Feelings_in_Russian_anime_Alya_character_renhder;
+            pictureBox1.Location = new Point(672, -3);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(337, 652);
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = Properties.Resources.filters_quality_95_format_webp___1_1;
+            pictureBox2.Location = new Point(-200, -48);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(614, 784);
+            pictureBox2.TabIndex = 3;
+            pictureBox2.TabStop = false;
+            // 
+            // bt_cadastrarCaixa
+            // 
+            bt_cadastrarCaixa.BackColor = SystemColors.ActiveCaptionText;
+            bt_cadastrarCaixa.FlatAppearance.BorderColor = Color.Red;
+            bt_cadastrarCaixa.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            bt_cadastrarCaixa.ForeColor = Color.Lime;
+            bt_cadastrarCaixa.Location = new Point(285, 225);
+            bt_cadastrarCaixa.Name = "bt_cadastrarCaixa";
+            bt_cadastrarCaixa.Size = new Size(194, 67);
+            bt_cadastrarCaixa.TabIndex = 4;
+            bt_cadastrarCaixa.Text = "CADASTRAR CAIXA";
+            bt_cadastrarCaixa.UseVisualStyleBackColor = false;
+            // 
+            // bt_cadastrarFornecedor
+            // 
+            bt_cadastrarFornecedor.BackColor = SystemColors.ActiveCaptionText;
+            bt_cadastrarFornecedor.FlatAppearance.BorderColor = Color.Red;
+            bt_cadastrarFornecedor.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            bt_cadastrarFornecedor.ForeColor = Color.Lime;
+            bt_cadastrarFornecedor.Location = new Point(285, 333);
+            bt_cadastrarFornecedor.Name = "bt_cadastrarFornecedor";
+            bt_cadastrarFornecedor.Size = new Size(194, 67);
+            bt_cadastrarFornecedor.TabIndex = 5;
+            bt_cadastrarFornecedor.Text = "CADASTRAR FORNECEDOR";
+            bt_cadastrarFornecedor.UseVisualStyleBackColor = false;
+            // 
+            // bt_cadastrarDespesa
+            // 
+            bt_cadastrarDespesa.BackColor = SystemColors.ActiveCaptionText;
+            bt_cadastrarDespesa.FlatAppearance.BorderColor = Color.Red;
+            bt_cadastrarDespesa.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            bt_cadastrarDespesa.ForeColor = Color.Lime;
+            bt_cadastrarDespesa.Location = new Point(285, 434);
+            bt_cadastrarDespesa.Name = "bt_cadastrarDespesa";
+            bt_cadastrarDespesa.Size = new Size(194, 67);
+            bt_cadastrarDespesa.TabIndex = 6;
+            bt_cadastrarDespesa.Text = "CADASTRAR DESPESA";
+            bt_cadastrarDespesa.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.DarkBlue;
+            button1.FlatAppearance.BorderColor = Color.Red;
+            button1.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.Gainsboro;
+            button1.Location = new Point(534, 125);
+            button1.Name = "button1";
+            button1.Size = new Size(183, 67);
+            button1.TabIndex = 7;
+            button1.Text = "CONSULTAR FUNCIONARIO";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.DarkBlue;
+            button2.FlatAppearance.BorderColor = Color.Red;
+            button2.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.ForeColor = Color.Gainsboro;
+            button2.Location = new Point(534, 225);
+            button2.Name = "button2";
+            button2.Size = new Size(183, 67);
+            button2.TabIndex = 8;
+            button2.Text = "CONSULTAR CAIXA";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.DarkBlue;
+            button3.FlatAppearance.BorderColor = Color.Red;
+            button3.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button3.ForeColor = Color.Gainsboro;
+            button3.Location = new Point(534, 333);
+            button3.Name = "button3";
+            button3.Size = new Size(183, 67);
+            button3.TabIndex = 9;
+            button3.Text = "CONSULTAR FORNECEDOR";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.DarkBlue;
+            button4.FlatAppearance.BorderColor = Color.Red;
+            button4.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button4.ForeColor = Color.Gainsboro;
+            button4.Location = new Point(534, 434);
+            button4.Name = "button4";
+            button4.Size = new Size(183, 67);
+            button4.TabIndex = 10;
+            button4.Text = "CONSULTAR DESPESA";
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(989, 616);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(bt_cadastrarDespesa);
+            Controls.Add(bt_cadastrarFornecedor);
+            Controls.Add(bt_cadastrarCaixa);
+            Controls.Add(label1);
+            Controls.Add(bt_cadastrarFuncionario);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button bt_cadastrarFuncionario;
+        private Label label1;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private Button bt_cadastrarCaixa;
+        private Button bt_cadastrarFornecedor;
+        private Button bt_cadastrarDespesa;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+    }
+}
